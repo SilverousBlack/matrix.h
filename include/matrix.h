f52d217c09a46551a9d526ab081b5a2bf7fa23af
SilverousBlack <juliancalebsegundo@gmail.com>
2019-09-11 21:29:48 +0800

    Further legacy updates

--- include/matrix.h
+++ include/matrix.h
@@ -1,4 +1,14 @@
 #pragma once
 #ifndef ___INC_MATRIX_
-#include "matpack.h"
+namespace experimental {
+    #include "matpack.h"
+}
+namespace legacy {
+    #include "previous/dimensional.h"
+    #include "previous/matrix.h"
+    template<typename type>
+    using legacy_matrix = s_mtx::matrix<type>;
+    template<size_t z = 0, size_t y = 0, size_t x = 0>
+    using legacy_dim_size = s_dim::dimensional_size<z, y, x>;
+}
 #endif
